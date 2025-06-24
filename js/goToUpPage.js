03e0c2221df30b4fb24587abb424d4d63cec966e
eternalhq7 <eternalhq7@foxmail.com>
2025-06-25 03:32:42 +0800

    Add files via upload

---
+++ js/goToUpPage.js
@@ -0,0 +1,46 @@
+function goToUpPage() {
+    // 跳转到上一页，这里是 security-info.html，根据实际文件名修改
+    window.location.href = 'security-info.html'; 
+}
+
+function goToUpPage1() {
+    // 跳转到上一页，这里是 security-info.html，根据实际文件名修改
+    window.location.href = '../battery-security.html'; 
+}
+
+function goToUpPage2() {
+    // 跳转到上一页，这里是 avalanche-backpack.html，根据实际文件名修改
+    window.location.href = '../avalanche-backpack.html'; 
+}
+
+function goToUpPage3() {
+    // 跳转到上一页，这里是 pet-transport.html，根据实际文件名修改
+    window.location.href = '../pet-transport.html'; 
+}
+
+function goToUpPage4() {
+    // 跳转到上一页，这里是 liquid-goods.html，根据实际文件名修改
+    window.location.href = '../liquid-goods.html'; 
+}
+
+function goToUpPage5() {
+    // 跳转到上一页，这里是 organ-transport.html，根据实际文件名修改
+    window.location.href = '../organ-transport.html'; 
+}
+
+
+//天山国际机场线路图返回上一页
+function GoToUpPage() {
+    // 跳转到上一页，这里是 terminal-building-info.html，根据实际文件名修改
+    window.location.href = 'airport-route.html'; 
+}
+
+function getToUpPage1() {
+    // 跳转到上一页，这里是 terminal-building-info.html，根据实际文件名修改
+    window.location.href = 'checkin-counter-info.html'; 
+}
+
+function getToUpPage2() {
+    // 跳转到上一页，这里是 terminal-building-info.html，根据实际文件名修改
+    window.location.href = 'gtc-transport-center.html'; 
+}
